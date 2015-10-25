b21e9daca0eae8d78fe33876b2103a98ea975d77
Glaider <avHITlearn@gmail.com>
2015-10-26 00:07:54 +0200

    new file

---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<50000;i++)
+            {
+                System.Console.WriteLine("Hii");
+            }
+                      
+        }
+    }
+}
